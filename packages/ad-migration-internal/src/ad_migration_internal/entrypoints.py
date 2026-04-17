375d12ac44ecc25d183c4b491b310ce6ac69b4c2
Hemanta Banerjee <hbanerjee@gmail.com>
2026-04-17 20:15:46 +0800

    chore: mark python console entrypoint exports

--- packages/ad-migration-internal/src/ad_migration_internal/entrypoints.py
+++ packages/ad-migration-internal/src/ad_migration_internal/entrypoints.py
@@ -1,12 +1,12 @@
 """Internal ad-migration entrypoints."""
 
-from shared.catalog_enrich import app as catalog_enrich_app
-from shared.discover import app as discover_app
-from shared.generate_sources import app as generate_sources_app
-from shared.init import app as init_app
-from shared.migrate import app as migrate_app
-from shared.dry_run import app as migrate_util_app
-from shared.profile import app as profile_app
-from shared.refactor import app as refactor_app
-from shared.setup_ddl import app as setup_ddl_app
-from shared.test_harness import app as test_harness_app
+from shared.catalog_enrich import app as catalog_enrich_app  # noqa: F401
+from shared.discover import app as discover_app  # noqa: F401
+from shared.generate_sources import app as generate_sources_app  # noqa: F401
+from shared.init import app as init_app  # noqa: F401
+from shared.migrate import app as migrate_app  # noqa: F401
+from shared.dry_run import app as migrate_util_app  # noqa: F401
+from shared.profile import app as profile_app  # noqa: F401
+from shared.refactor import app as refactor_app  # noqa: F401
+from shared.setup_ddl import app as setup_ddl_app  # noqa: F401
+from shared.test_harness import app as test_harness_app  # noqa: F401
